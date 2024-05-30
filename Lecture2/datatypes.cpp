eb04dac557aeb3ede966d92327abc4592cd2bd0d
Ankit Kumar Biswal <116702754+AnkitkumarBiswal04@users.noreply.github.com>
2024-05-30 18:11:20 +0530

    Create datatypes.cpp

---
+++ Lecture2/datatypes.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+// in this cod we learnt about the datatypes ,
+//variables and the size of the datatypes 
+int main(){
+    int a=123;
+    cout<<a<<" "<<"size of int is:"<<sizeof(a)<<endl;
+    char c='v';
+    cout<<c<<" "<<" size of char is:"<<sizeof(c)<<endl;
+    bool bl="true";
+    cout<<bl<<" "<<"size of bool is:"<<sizeof(bl)<<endl;
+    float f=1.2;
+    cout<<f<<" "<<"size of float is:"<<sizeof(f)<<endl;
+    double d=1.23;
+    cout<<d<<" "<<"size of double is:"<<sizeof(d)<<endl;
+}
