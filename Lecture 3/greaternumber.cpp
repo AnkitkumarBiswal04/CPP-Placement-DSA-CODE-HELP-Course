0a91ed0c8e0ec2d9c8ce47c25c547bdb3579eead
Ankit Kumar Biswal <116702754+AnkitkumarBiswal04@users.noreply.github.com>
2024-05-31 01:05:33 +0530

    Create greaternumber.cpp

---
+++ Lecture 3/greaternumber.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+//this code is to check whether one number is greater than other
+int main(){
+    int a,b;
+    cin>>a>>b;
+    if(a>b){
+        cout<<"a is greater than b"<<endl;
+    }
+    if(b>a){
+        cout<<"b is greater than a"<<endl;
+    }
+    return 0;
+}
