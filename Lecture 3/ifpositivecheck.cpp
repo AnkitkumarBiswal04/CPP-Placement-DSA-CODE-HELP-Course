3263518e0dbe12e0489c2c4695614f46623ed0e4
Ankit Kumar Biswal <116702754+AnkitkumarBiswal04@users.noreply.github.com>
2024-05-31 00:57:03 +0530

    Create ifpositivecheck.cpp

---
+++ Lecture 3/ifpositivecheck.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+//this code is to check whether the code is positive or negative
+int main(){
+    int n;
+    cin>>n;
+    if(n>0){
+        cout<<"the number is positive"<<endl;
+    }
+    else{
+        cout<<"the number is negative"<<endl;
+    }
+    return 0;
+}
