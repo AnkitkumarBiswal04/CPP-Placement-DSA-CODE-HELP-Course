12deb37f0e8710d7c96555e835f26b740b802355
Ankit Kumar Biswal <116702754+AnkitkumarBiswal04@users.noreply.github.com>
2024-06-01 20:09:48 +0530

    Create printnumbertilln.cpp

---
+++ Lecture 3/printnumbertilln.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+//this code is to print number from 1 to n
+int main(){
+    int n;
+    cin>>n;
+    int i=1;
+    while(i<=n){
+        cout<<i<<endl;
+        i=i+1;
+    }
+    return 0;
+}
