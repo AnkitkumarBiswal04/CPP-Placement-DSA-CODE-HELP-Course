ab0fc7deef3eab5bef192a97d3f7e73957e3edc2
Ankit Kumar Biswal <116702754+AnkitkumarBiswal04@users.noreply.github.com>
2024-06-01 21:48:10 +0530

    Create celciusfarenheit.cpp

---
+++ Lecture 3/celciusfarenheit.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+//this code is to find the celcius and farenheit
+int main(){
+    float f;
+    cin>>f;
+    int c=(5.0/9)*(f-32);
+    cout<<c<<endl;
+    return 0;
+}
