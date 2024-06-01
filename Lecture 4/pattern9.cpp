ff36f04bdc9e5abfa959a53569d7e25d28a68c78
Ankit Kumar Biswal <116702754+AnkitkumarBiswal04@users.noreply.github.com>
2024-06-02 02:14:10 +0530

    Create pattern9.cpp

---
+++ Lecture 4/pattern9.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+//this code is to print this pattern 
+// 1 
+// 2 1 
+// 3 2 1 
+// 4 3 2 1 
+int main(){
+    int n;
+    cin>>n;
+    int i=1;
+    while(i<=n){
+        int j=1;
+        while(j<=i){
+            cout<<(i-j+1)<<" ";
+            j=j+1;
+        }
+        cout<<endl;
+        i=i+1;
+    }
+    return 0;
+}
