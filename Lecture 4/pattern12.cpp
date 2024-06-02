2d58ef9bdc2d2c673865fb3ab459425bc2fbea03
Ankit Kumar Biswal <116702754+AnkitkumarBiswal04@users.noreply.github.com>
2024-06-02 10:49:39 +0530

    Create pattern12.cpp

---
+++ Lecture 4/pattern12.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+//this code is to print pattern like
+// A B C 
+// D E F 
+// G H I 
+int main(){
+    int n;
+    cin>>n;
+    int row=1;
+    int val=1;
+    while (row<=n){
+        int col=1;
+        while(col<=n){
+            char ch='A'+val-1;
+            cout<<ch<<" ";
+            val=val+1;
+            col=col+1;
+        }
+    cout<<endl;
+    row=row+1;
+    }
+
+    return 0;
+}
