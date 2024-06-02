07eef721eaf6c55de21e295331ed96bc9117bd89
Ankit Kumar Biswal <116702754+AnkitkumarBiswal04@users.noreply.github.com>
2024-06-02 15:30:42 +0530

    Create printusingfortilln.cpp

---
+++ Lecture 5/printusingfortilln.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+//this code is to print number from 1 to n using for loop
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        
+        cout<<i<<endl;
+    }
+    return 0;
+}
