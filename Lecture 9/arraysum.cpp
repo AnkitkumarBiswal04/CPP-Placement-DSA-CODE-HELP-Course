81406a3934af87f950eac24b350c4ce0d8b77283
Ankit Kumar Biswal <116702754+AnkitkumarBiswal04@users.noreply.github.com>
2024-06-05 20:50:58 +0530

    Create arraysum.cpp

---
+++ Lecture 9/arraysum.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+//this code is to sfind sum in an array
+int sumArray(int array[],int size){
+    int sum=0;
+    for(int i=0;i<size;i++){
+        sum=sum+array[i];
+    }
+    return sum;
+}
+int main(){
+    int size;
+    cin>>size;
+    int array[size];
+    for(int i=0;i<size;i++){
+        cin>>array[i];
+    }
+    cout<<"the sum of array is:"<<sumArray(array,size);
+    return 0;
+}
