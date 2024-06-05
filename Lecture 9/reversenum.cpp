d03ec7e338af6e23f684b3a04ec32410f6884fc4
Ankit Kumar Biswal <116702754+AnkitkumarBiswal04@users.noreply.github.com>
2024-06-05 23:49:13 +0530

    Create reversenum.cpp

---
+++ Lecture 9/reversenum.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+//this code is to print reverse of a number
+void reverse(int arr[],int n){
+    int start=0;
+    int end=n-1;
+    while(start<=end){
+        swap(arr[start],arr[end]);
+        start++;
+        end--;
+    }   
+    cout<<"the reverse number is :";
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    reverse(arr,n);
+}
