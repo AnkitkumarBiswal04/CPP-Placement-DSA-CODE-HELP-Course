1aefdc71fe91dede1faa1ded52a116dfb6aa0b2c
Ankit Kumar Biswal <116702754+AnkitkumarBiswal04@users.noreply.github.com>
2024-09-06 23:24:42 +0530

    Create check_string_palindrome.cpp

---
+++ Lecture 22/check_string_palindrome.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+bool palindrome(char name[],int n){
+    int start=0;
+    int end=n-1;
+    while(start<=end){
+        if(name[start]!=name[end]){
+            return 0;
+        }
+        else{
+            start++;
+            end--;
+        }
+    }
+    return 1;
+}
+void reverse(char name[],int n){
+    int start=0;
+    int end=n-1;
+    while(start<end){
+        swap(name[start++],name[end--]);
+    }
+}
+int length(char name[]){
+    int count=0;
+    for(int i=0;name[i]!='\0';i++){
+        count++;
+    }
+    return count;
+}
+int main(){
+    char name[20];
+    cout<<"Enter the name :"<<endl;
+    cin>>name;
+    cout<<"Your name is :";
+    cout<<name<<endl;
+    int len=length(name);
+    
+    reverse(name,len);
+    cout<<"Your reverse name is :";
+    cout<<name<<endl;
+    
+    
+    cout<<"palindrome or not true or false:"<<palindrome(name,len);;
+    return 0;
+}
